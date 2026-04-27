7e66c6102b2181cc526395d5e9ce88d45b17c6f2
JERISH-C-MOSES <jerishcmoses6@gmail.com>
2026-04-27 21:26:03 +0530

    Fixed remaining JUnit import

--- Maven_Project/src/main/java/com/utility/FileReaderManager.java
+++ Maven_Project/src/main/java/com/utility/FileReaderManager.java
@@ -6,7 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
-import org.junit.Assert;
+import org.testng.Assert;
 
 
 public class FileReaderManager 
